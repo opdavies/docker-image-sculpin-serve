0813345363ca32cc3f73df11e732d787a75858ae
Oliver Davies <oliver@oliverdavies.uk>
2021-08-19 23:37:48 +0100

    ci: Allow for tagging a commit SHA

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+if [[ $# -eq 1 ]]; then
+  sha=$1
+else
+  sha=latest
+fi
+
 # Docker Hub
-docker image build . -t opdavies/sculpin --target sculpin
-docker image build . -t opdavies/sculpin-serve --target sculpin-serve
-docker image push opdavies/sculpin
-docker image push opdavies/sculpin-serve
+docker image build . -t opdavies/sculpin:$sha --target sculpin
+docker image build . -t opdavies/sculpin-serve:$sha --target sculpin-serve
+docker image push opdavies/sculpin:$sha
+docker image push opdavies/sculpin-serve:$sha
 
 # GitHub
-docker image build . -t ghcr.io/opdavies/sculpin --target sculpin
-docker image build . -t ghcr.io/opdavies/sculpin-serve --target sculpin-serve
-docker image push ghcr.io/opdavies/sculpin
-docker image push ghcr.io/opdavies/sculpin-serve
+docker image build . -t ghcr.io/opdavies/sculpin:$sha --target sculpin
+docker image build . -t ghcr.io/opdavies/sculpin-serve:$sha --target sculpin-serve
+docker image push ghcr.io/opdavies/sculpin:$sha
+docker image push ghcr.io/opdavies/sculpin-serve:$sha
