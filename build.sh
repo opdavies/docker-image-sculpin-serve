63d2e130c407ad332834e18488e498199cfabc52
Oliver Davies <oliver@oliverdavies.uk>
2021-08-19 23:30:35 +0100

    ci: Push to the GitHub container registry

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Docker Hub
 docker image build . -t opdavies/sculpin --target sculpin
 docker image build . -t opdavies/sculpin-serve --target sculpin-serve
-
 docker image push opdavies/sculpin
 docker image push opdavies/sculpin-serve
+
+# GitHub
+docker image build . -t ghcr.io/opdavies/sculpin --target sculpin
+docker image build . -t ghcr.io/opdavies/sculpin-serve --target sculpin-serve
+docker image push ghcr.io/opdavies/sculpin
+docker image push ghcr.io/opdavies/sculpin-serve
